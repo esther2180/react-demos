c441377198931bd3952449c9f04302f5b5dbfcd4
Esther <esther2180@gmail.com>
2018-09-15 22:42:07 -0700

    React - Created a Clock

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import Greeting from './greeting';
+import Clock from './clock';
 
+// Greeting Demo
+// const App = () => (
+//     <div>
+//         <div className="app">
+//             <Greeting name="Esther" favColor="Pink" />
+//             <Greeting name="Invisible Man" favColor="purple" number={7}/>
+//                 <img src={logo} className="logo rotate"/>
+//                 <h1>Welcome to React</h1>
+//         </div>
+//     </div>
+// );
+
+// export default App;
+
+
+// Clock Demo
 const App = () => (
     <div>
         <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
+            <Clock />
         </div>
     </div>
 );
---
+++ src/components/clock.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import Time from './time';
+
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            time: new Date()
+        }
+    }
+
+    componentWillMount() {
+        setInterval(() => this.tick(), 1000);
+    }
+
+    tick() {
+        this.setState({
+            time: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>The time is: {this.state.time.toLocaleTimeString()}</h1>
+            </div>
+        );
+    }
+}
+
+export default Clock;
